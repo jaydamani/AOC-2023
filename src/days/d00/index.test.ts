69f86f87a7a4f68d70ff7dd7ed6ddbbaa6a322fc
jaydamani <jaydamani567@gmail.com>
2023-12-03 21:02:20 +0530

    refactor: update template

--- src/days/d00/index.test.ts
+++ src/days/d00/index.test.ts
@@ -1,11 +1,25 @@
-// sum.test.js
 import { expect, test } from "vitest";
-import { solvePart1 } from "./index";
 import { basename } from "path";
+import {
+  solvePart1,
+  // solvePart2,
+  // solvePart3
+} from "./index";
 
 const part1In = `test`;
 const part1Ans = `test`;
-
-test("solution for " + basename(__dirname), () => {
+test("solution for " + basename(__dirname) + " part 1", () => {
   expect(solvePart1(part1In)).toBe(part1Ans);
 });
+
+// const part2In = `test`;
+// const part2Ans = `test`;
+// test("solution for " + basename(__dirname) + " part 2", () => {
+//   expect(solvePart2(part2In)).toBe(part2Ans);
+// });
+
+// const part3In = `test`;
+// const part3Ans = `test`;
+// test("solution for " + basename(__dirname) + " part 3", () => {
+//   expect(solvePart3(part2In)).toBe(part3Ans);
+// });
