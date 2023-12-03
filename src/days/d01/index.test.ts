512e9a85bf3fdd2a2a0e01bf8cce37fa6559b468
jaydamani <jaydamani567@gmail.com>
2023-12-04 00:39:56 +0530

    feat: init day 1 part 2

--- src/days/d01/index.test.ts
+++ src/days/d01/index.test.ts
@@ -1,7 +1,10 @@
-// sum.test.js
 import { expect, test } from "vitest";
-import { solvePart1 } from "./index";
 import { basename } from "path";
+import {
+  solvePart1,
+  solvePart2,
+  // solvePart3
+} from "./index";
 
 const part1In = `1abc2
 pqr3stu8vwx
@@ -9,7 +12,26 @@ a1b2c3d4e5f
 treb7uchet
 `;
 const part1Ans = `142`;
-
-test("solution for " + basename(__dirname).replace(/d0?/, "day "), () => {
+test("solution for " + basename(__dirname) + " part 1", () => {
   expect(solvePart1(part1In)).toBe(part1Ans);
 });
+
+const part2In = `
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+`;
+const part2Ans = `281`;
+test("solution for " + basename(__dirname) + " part 2", () => {
+  expect(solvePart2(part2In)).toBe(part2Ans);
+});
+
+// const part3In = `test`;
+// const part3Ans = `test`;
+// test("solution for " + basename(__dirname) + " part 3", () => {
+//   expect(solvePart3(part2In)).toBe(part3Ans);
+// });
--- src/days/d01/index.ts
+++ src/days/d01/index.ts
@@ -6,3 +6,26 @@ export function solvePart1(input: string) {
     .reduce((a, b) => a + Number(b[0] + b.at(-1)), 0)
     .toString();
 }
+
+const replacements = {
+  zero: "0o",
+  one: "o1e",
+  two: "t2o",
+  three: "t3e",
+  four: "4",
+  five: "5e",
+  six: "6",
+  seven: "7n",
+  eight: "e8t",
+  nine: "n9e",
+};
+
+export function solvePart2(input: string) {
+  for (const word in replacements) {
+    input = input.replaceAll(
+      word,
+      replacements[word as keyof typeof replacements]
+    );
+  }
+  return solvePart1(input);
+}
