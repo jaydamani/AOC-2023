2ad7919b3f5ffdee912d4c06c0a22ce64105bba6
jaydamani <jaydamani567@gmail.com>
2023-12-06 00:15:57 +0530

    feat: solve day 3 part 2

--- src/days/d03/index.test.ts
+++ src/days/d03/index.test.ts
@@ -2,27 +2,34 @@ import { expect, test } from "vitest";
 import { basename } from "path";
 import {
   solvePart1,
-  // solvePart2,
+  solvePart2,
   // solvePart3
 } from "./index";
 
 const part1In = `
-****************************
-....**.....*************....
-111.**.102.*************.111
-....**.....*************....
-****************************
+12.......*..
++.........34
+.......-12..
+..78........
+..*....60...
+78..........
+.......23...
+....90*12...
+............
+2.2......12.
+.*.........*
+1.1.......56
 `;
-const part1Ans = `0`;
+const part1Ans = `413`;
 test("solution for " + basename(__dirname) + " part 1", () => {
   expect(solvePart1(part1In)).toBe(part1Ans);
 });
 
-// const part2In = `test`;
-// const part2Ans = `test`;
-// test("solution for " + basename(__dirname) + " part 2", () => {
-//   expect(solvePart2(part2In)).toBe(part2Ans);
-// });
+const part2In = part1In;
+const part2Ans = "6756";
+test("solution for " + basename(__dirname) + " part 2", () => {
+  expect(solvePart2(part2In)).toBe(part2Ans);
+});
 
 // const part3In = `test`;
 // const part3Ans = `test`;
--- src/days/d03/index.ts
+++ src/days/d03/index.ts
@@ -3,6 +3,47 @@ const nonSymbol = /[0-9\.]/;
 export function solvePart1(input: string) {
   let total = 0;
   const inputArr = input.split("\n");
+  for (const { i, j, num } of getNumbers(inputArr)) {
+    if (!hasAdjacentSymbols(inputArr, i, j, num.length)) continue;
+    total += +num;
+  }
+  return total.toString();
+}
+
+export function solvePart2(input: string) {
+  const gearMap: Record<string, number[]> = {};
+  const inputArr = input.split("\n");
+  for (const { i: row, j: column, num } of getNumbers(inputArr)) {
+    checkGear(inputArr, row, column, +num, gearMap);
+    checkGear(inputArr, row, column - num.length - 1, +num, gearMap);
+
+    for (let i = -1; i < num.length + 1; i++) {
+      checkGear(inputArr, row - 1, column + i - num.length, +num, gearMap);
+      checkGear(inputArr, row + 1, column + i - num.length, +num, gearMap);
+    }
+  }
+  let total = 0;
+  console.log(gearMap);
+  for (const nums of Object.values(gearMap)) {
+    if (nums.length > 1) total += nums.reduce((a, b) => a * b);
+  }
+  return total.toString();
+}
+
+function checkGear(
+  input: string[],
+  row: number,
+  column: number,
+  num: number,
+  gearMap: Record<string, number[]>
+) {
+  if (row < 0 || row >= input.length) return;
+  if (column < 0 || column >= input[row].length) return;
+  if (input[row][column] !== "*") return;
+  (gearMap[row + "|" + column] ??= []).push(num);
+}
+
+function* getNumbers(inputArr: string[]) {
   for (let i = 0; i < inputArr.length; i++) {
     for (let j = 0; j < inputArr[i].length; j++) {
       let char = inputArr[i][j];
@@ -16,11 +57,9 @@ export function solvePart1(input: string) {
         char = inputArr[i][j];
       }
       if (!num.length) continue;
-      if (!hasAdjacentSymbols(inputArr, i, j, num.length)) continue;
-      total += +num;
+      yield { num, i, j };
     }
   }
-  return total.toString();
 }
 
 function hasAdjacentSymbols(
